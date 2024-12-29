76369af664969a590477db2531e860e289ea4d2e
Conan Torres <conantorreswf@gmail.com>
2024-12-29 11:02:59 -0300

    feat: adiciona declaração de retorno da View

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Contracts\View\View;
 
-Route::get('/', function () {
+Route::get('/', function (): View {
     return view('login');
 })->name('login');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/register', function () {
+Route::get('/register', function (): View {
     return view('register');
 })->name('register');
 
-Route::get('/users', function () {
+Route::get('/users', function (): View {
     return view('users');
 })->name('users');
