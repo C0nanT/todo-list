6542aad744880d35a0fb050eaa2418c6c8386b63
Conan Torres <conantorreswf@gmail.com>
2024-12-29 08:23:27 -0300

    feat: adiciona footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto pt-3 bg-dark text-light">
+    <div class="container">
+        <a href="https://github.com/C0nanT" target="_blank" class="text-muted">github.com/C0nanT</a>
+    </div>
+</footer>
